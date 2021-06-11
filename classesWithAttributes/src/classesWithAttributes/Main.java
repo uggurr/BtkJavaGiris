31c529ca050e1a6ef53e9510835a1c44061fa503
uggurr <uggurryildiz@gmail.com>
2021-06-11 08:07:58 +0300

    AttributeClasses

---
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,18 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		product.id=1;
+		product.name="laptop";
+		product.description="Asus laptop";
+		product.price=5000;
+		product.stockAmount=3;
+		
+		ProductManager productManager = new ProductManager();
+		productManager.add(product);
+
+	}
+
+}
