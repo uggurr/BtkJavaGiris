a80aa513b8e8eba78ea85bcf93c8fc257ff87e2a
uggurr <uggurryildiz@gmail.com>
2021-06-11 14:54:30 +0300

    GetterVeSetterConstructor

--- classesWithAttributes/src/classesWithAttributes/Main.java
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -3,12 +3,17 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Product product1 = new Product(1,"Telefon","Samsung Telefon",3000,5);
+		
+		System.out.println(product1.getName());
+		
 		Product product = new Product();
-		product.id=1;
-		product.name="laptop";
-		product.description="Asus laptop";
-		product.price=5000;
-		product.stockAmount=3;
+		product.setId(1);
+		product.setName("Laptop");
+		product.setDescription("Hp Laptop");;
+		product.setPrice(5000);;
+		product.setStockAmount(3);;
 		
 		ProductManager productManager = new ProductManager();
 		productManager.add(product);
--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -2,6 +2,7 @@ package classesWithAttributes;
 
 public class ProductManager {
 	public void add(Product product) {
-		System.out.println("Ürün eklendi " + product.name);
+		System.out.println("Ürün eklendi " + product.getName());
 	}
+	public void add2() {}
 }
