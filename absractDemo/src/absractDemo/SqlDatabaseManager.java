a0003cd0f4d29d9daea2855017b47e8d0b2f3c94
uggurr <uggurryildiz@gmail.com>
2021-06-11 16:56:32 +0300

    abstractDemo

---
+++ absractDemo/src/absractDemo/SqlDatabaseManager.java
@@ -0,0 +1,11 @@
+package absractDemo;
+
+public class SqlDatabaseManager extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Veri getirildi : Sql");
+		
+	}
+
+}
