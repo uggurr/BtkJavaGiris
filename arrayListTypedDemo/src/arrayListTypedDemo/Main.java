5dea192d60d550290b65b757ee5be57e4befb449
uggurr <uggurryildiz@gmail.com>
2021-06-17 02:50:37 +0300

    ArrayListType

---
+++ arrayListTypedDemo/src/arrayListTypedDemo/Main.java
@@ -0,0 +1,15 @@
+package arrayListTypedDemo;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<String> sehirler = new ArrayList<String>();
+		sehirler.add("Ankara");
+		sehirler.add("Ýstanbul");
+		//sehirler.add(1); -> String tipinde deðer istediði için int tipinde bir deðer verirsem hata verir.
+
+	}
+
+}
