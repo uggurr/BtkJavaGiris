d49ba08d77cf98ec334c54493530217f2d38abe1
uggurr <uggurryildiz@gmail.com>
2021-06-17 15:29:50 +0300

    threadDemo

---
+++ threadingDemo/src/threadingDemo/KronometreThread.java
@@ -0,0 +1,39 @@
+package threadingDemo;
+
+
+
+public class KronometreThread implements Runnable{
+	
+	private Thread thread;
+	private String threadName;
+	
+	public  KronometreThread(String threadName) {
+		this.threadName = threadName;
+		System.out.println("oluþturuluyor - " + threadName);
+	}
+	@Override
+	public void run() {
+		System.out.println("Çalýþtýrýlýyor - " + threadName);
+		
+		try {
+			for(int i= 1;i<=10;i++) {
+				System.out.println(threadName +  " : " + i);
+				Thread.sleep(1000);
+			}
+		}catch(InterruptedException exception){
+			System.out.println("Kesildi - " + threadName);
+		}
+		
+		System.out.println("Thread bitti - " + threadName);
+		
+	}
+	
+	public void start() {
+		System.out.println("Thread nesnesi oluþuyor");
+		if (thread==null) {
+			thread = new Thread(this,threadName);
+			thread.start();
+		}
+	}
+
+}
